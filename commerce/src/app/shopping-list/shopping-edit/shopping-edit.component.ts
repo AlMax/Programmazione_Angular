5c88207a0eccd6f3f3f276e87bf935402176beb7
AlMax <alihaider.maqsood@gmail.com>
2021-01-21 17:54:25 +0100

    esercizi

--- commerce/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ commerce/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  inputNome: string;
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit(): void {
+  }
+
+  aggiungi(){
+    alert(this.name)
   }
 
 }
